9ff31192406bfe90fb500ecb394c14ce44d6dd7e
Todd Morse <todd@morsecodist.io>
2022-05-07 12:01:53 -0400

    discord + navbar

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -29,6 +29,8 @@
 
 <h1 class="text-primary">Welcome to Livable Portland!</h1>
 
+<p>Join us on <a href="https://discord.gg/EBF8EUN2RK" target="_blank">Discord</a>!</p>
+
 <p>Livable Portland is a political group that looks to make Portland, Maine more livable for everyone. We believe in making that happen through supporting middle density mixed-use communities. Livable Portland is non-partisian and we believe that our policies can appeal to people wherever they are on the political spectrum. Our policies and approach are strongly inspired by the YIMBY movement, Strong Towns, and more. If you like building housing and think people shouldn't need to drive everywhere you will fit right in. Our big ideas are:</p>
 
 <div class="row g-4 pb-4">
