4360f1c26b3ecfb6978cf1c4d3b3a5a5323fb972
Todd Morse <todd@morsecodist.io>
2023-01-14 23:17:30 -0500

    fix google auth bug

--- src/routes/events.json.ts
+++ src/routes/events.json.ts
@@ -3,9 +3,10 @@ import { OAuth2Client } from 'google-auth-library'
 
 async function listEvents() {
     const credentials = JSON.parse(Buffer.from(process.env.GOOGLE_CALENDAR_KEY, 'base64').toString('utf-8'));
-    const client = new OAuth2Client()
-    client.setCredentials(credentials)
-    client.forceRefreshOnFailure = true
+    const client = new OAuth2Client(process.env.GOOGLE_AUTH_CLIENT_ID, process.env.GOOGLE_AUTH_CLIENT_SECRET)
+    client.setCredentials({
+        refresh_token: credentials.refresh_token,
+    })
     const calendar = google.calendar({ version: 'v3', auth: client });
     const res = await calendar.events.list({
         calendarId: process.env.CALENDAR_ID,
