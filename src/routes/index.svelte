20b7753baaa50e81c9d4dd5e357546602e4db18c
Todd Morse <todd@morsecodist.io>
2023-06-04 11:47:07 -0400

    typo

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -55,7 +55,7 @@
 <div class="d-flex flex-wrap">
     <div class="pe-4 flex-grow-1 flex-shrink-1" style="min-width: 300px; width: 300px;">
         <h2 class="text-start text-secondary">Who we are</h2>
-        <p class="text-start">The Urbanist Coalition of Portland (formerly Livable Portland) is a political group with the goal of making Portland, Maine more livable for everyone. We recently changed our name from "Livable Portland" to avoid confusion with the Maine DSA's campaign of the same name. We support middle density, mixed-use neighborhoods, with access to transit and cycling infrastructure. This will provide affordable housing, protect our environment, and overall make our city a more pleasant and safe place to live and work . Our policies and approach are strongly inspired by the YIMBY movement, Strong Towns, and more. If you think building housing is a good thing and that shouldn't need to drive everywhere you will fit right in.
+        <p class="text-start">The Urbanist Coalition of Portland (formerly Livable Portland) is a political group with the goal of making Portland, Maine more livable for everyone. We recently changed our name from "Livable Portland" to avoid confusion with the Maine DSA's campaign of the same name. We support middle density, mixed-use neighborhoods, with access to transit and cycling infrastructure. This will provide affordable housing, protect our environment, and overall make our city a more pleasant and safe place to live and work . Our policies and approach are strongly inspired by the YIMBY movement, Strong Towns, and more. If you think building housing is a good thing and that you shouldn't need to drive everywhere you will fit right in.
 </p>
     </div>
     <div class="flex-shrink-0 flex-grow-2 text-start ">
