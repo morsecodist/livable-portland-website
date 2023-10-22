aa6ab6890b5d8dd02eabd499711d97d923fdfd48
Todd Morse <todd@morsecodist.io>
2023-10-22 13:51:58 -0400

    yaml parse

--- src/routes/campaigns/[slug].json.ts
+++ src/routes/campaigns/[slug].json.ts
@@ -1,5 +1,5 @@
 import fs from "fs";
-import { parse } from "yaml";
+import YAML from "yaml";
 import { resolve, join } from "path";
 
 function readFile(path: fs.PathLike): Promise<Buffer> {
@@ -14,7 +14,7 @@ function readFile(path: fs.PathLike): Promise<Buffer> {
 export async function get({ params }) {
     const { slug } = params;
     const file = join(resolve(), `content/campaigns/${slug}.yml`);
-    const obj = parse((await readFile(file)).toString());
+    const obj = YAML.parse((await readFile(file)).toString());
 
     // remap dateTime in each object to a Date object
     let body = obj.map((event: any) => {
