2cf13cb1bae73e01e1743563059d2e69577b7286
Todd Morse <todd@morsecodist.io>
2023-11-05 23:44:49 -0500

    test

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,12 +1,11 @@
 export async function handle({ event, resolve }) {
-  const oldBase = 'https://livableportland.me';
-  const newBase = 'https://urbanistportland.me';
+  const oldBase = 'livableportland.me';
+  const newBase = 'urbanistportland.me';
   
   // Check if the request is for the old domain
-  if (event.request.headers.host === oldBase) {
-    // Redirect to the new domain while preserving the path and query
-    const newUrl = newBase + event.request.path + (event.request.query ? '?' + event.request.query : '');
-    return Response.redirect(newUrl, 301); // Permanent redirect
+  if (event.url.host === oldBase) {
+    event.url.host = newBase;
+    return Response.redirect(event.url.href, 301); // Permanent redirect
   }
 
   // Proceed with the regular request handling if the host doesn't match the old domain
