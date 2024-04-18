49c32513b216c3c985a8e984fab3050c0924abac
Todd Morse <todd@morsecodist.io>
2024-04-18 11:55:04 -0400

    leviathan draft one

---
+++ src/routes/upcoming-events/portland-leviathan.svelte
@@ -0,0 +1,99 @@
+<script>
+	import MailingList from '../../components/MailingList.svelte';
+</script>
+
+<svelte:head>
+	<title>UCP - Portland Leviathant</title>
+</svelte:head>
+
+<h1 class="text-primary">Portland Leviathan</h1>
+<p>Conference on the Past, Present, & Future of Interstate 295</p>
+
+<img src="/leviathan.png" alt="295 Leviathan" />
+
+<div class="text-start">
+	<p>
+		In the 20<sup>th</sup> century, cities across America weres carred by urban highways – and Portland
+		Maine is no exception. Here, I-295 was routed through underserved neighborhoods and communities of
+		color permanently altering the city's landscape. During this time, the city also experienced the
+		destruction of the iconic Union Station, the demolition of a thriving housing corporation, and the
+		eradication of Maine's largest African American community at the time.
+	</p>
+
+	<p>
+		Today, Portlanders are working to undo the damage and restore our city. Many citizen campaigns
+		have centered around reckoning with the negative legacy of I-295, by advocating for the
+		restoration of a walkable human scale city and the corollary benefits of environmental justice,
+		economic development, vibrancy and beauty.
+	</p>
+
+	<p>
+		Our efforts have been largely supported by the city government, which is seeking two
+		<b>Reconnecting Communities</b> grants from the U.S. Department of Transportation to repair the neighborhood
+		fabric at Franklin Street and at exit 5. However,as advocates work away along the margins, the root
+		problem, I-295 itself, is often regarded as eternal and immutable.
+	</p>
+
+	<p class="lead pt-2">
+		Portland Leviathan 2024 will look beyond isolated projects to envision a bolder, better future
+		for the I-295 corridor, and our city as a whole.
+	</p>
+
+	<p class="fst-italic">
+		Schedule, presenters, and partners are provisional and subject to change.
+	</p>
+
+	<h2 class="text-primary">Program</h2>
+
+	<p>
+		<b class="text-uppercase">Portland Leviathan 2024</b> will feature talks and discussions considering
+		the broad context of urban highways in America, I-295 in particular, the effects on neighborhoods
+		and communities, an overview of Street Light and other data collection methods, current projects,
+		and plans for the future. There will also be a day time tour of the highway from bridge to bridge,
+		and reception to follow. The conference will take place in Spring 2024, exact date to be determined,
+		at the Jewish Community Alliance.
+	</p>
+
+	<h2 class="text-primary">Presenters</h2>
+
+	<ul class="list-group">
+		<li class="list-group-item border-0">
+			<div class="fw-bold">Ian Jacob</div>
+			<span class="fst-italic">Moderator</span>
+		</li>
+		<li class="list-group-item border-0">
+			<div class="fw-bold">Pending Representative</div>
+			<span class="fst-italic">United States Department of Transportation</span>
+		</li>
+		<li class="list-group-item border-0">
+			<div class="fw-bold">Fred Salvucci</div>
+			<span class="fst-italic">Visionary Engineer of the Big Dig</span>
+		</li>
+		<li class="list-group-item border-0">
+			<div class="fw-bold">Julie Lary</div>
+			<span class="fst-italic">Historian, Black Diamond</span>
+		</li>
+		<li class="list-group-item border-0">
+			<div class="fw-bold">Community Representatives</div>
+			<span class="fst-italic">and more</span>
+		</li>
+	</ul>
+
+	<h2 class="text-primary">Partner Organizations</h2>
+
+	<ul>
+		<li>Portland Society of Architecture</li>
+		<li>Libbytown Neighborhood Association</li>
+		<li>Franklin Reclamation Authority</li>
+		<li>Portland Trails</li>
+		<li>Urbanist Coalition of Portland</li>
+		<li>and more</li>
+	</ul>
+
+	<h2 class="text-primary">Sign Up to Hear More!</h2>
+	<p>
+		The mailing list for this event is independent of the UCP mailing list. By signing up here you
+		will only receive emails related to this event.
+	</p>
+	<MailingList listName="portland-leviathan" />
+</div>
