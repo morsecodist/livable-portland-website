cd67986209b7ce7192ff165effe2cf4c776ab3cc
Todd Morse <todd@morsecodist.io>
2024-05-01 12:54:11 -0400

    copy

--- src/routes/upcoming-events/portland-leviathan.svelte
+++ src/routes/upcoming-events/portland-leviathan.svelte
@@ -13,30 +13,30 @@
 
 <div class="text-start">
 	<p>
-		In the 20<sup>th</sup> century, cities across America weres carred by urban highways – and Portland
-		Maine is no exception. Here, I-295 was routed through underserved neighborhoods and communities of
-		color permanently altering the city's landscape. During this time, the city also experienced the
-		destruction of the iconic Union Station, the demolition of a thriving housing corporation, and the
-		eradication of Maine's largest African American community at the time.
+		In the 20th century, cities across America were scarred by urban highways – and Portland Maine
+		is no exception. Just as elsewhere, I-295 was routed through underserved neighborhoods and
+		communities of color permanently altering the city's landscape. During this time, the city also
+		experienced the destruction of the iconic Union Station, the demolition of a thriving housing
+		corporation, and the eradication of Maine's largest African American community at the time.
 	</p>
 
 	<p>
 		Today, Portlanders are working to undo the damage and restore our city. Many citizen campaigns
-		have centered around reckoning with the negative legacy of I-295, by advocating for the
-		restoration of a walkable human scale city and the corollary benefits of environmental justice,
-		economic development, vibrancy and beauty.
+		have centered around reckoning with the legacy of I-295, by advocating for the restoration of a
+		walkable human scale city and the corollary benefits of environmental justice, economic
+		development, vibrancy, and beauty.
 	</p>
 
 	<p>
-		Our efforts have been largely supported by the city government, which is seeking two
-		<b>Reconnecting Communities</b> grants from the U.S. Department of Transportation to repair the neighborhood
-		fabric at Franklin Street and at exit 5. However,as advocates work away along the margins, the root
-		problem, I-295 itself, is often regarded as eternal and immutable.
+		The efforts have been largely supported by the city government, which is seeking two
+		Reconnecting Communities grants from the U.S. Department of Transportation to repair the
+		neighborhood fabric at Franklin Street and at exit 5. However, as advocates work along the
+		margins, the root problem, I-295 itself, is often regarded as eternal and immutable.
 	</p>
 
 	<p class="lead pt-2">
 		Portland Leviathan 2024 will look beyond isolated projects to envision a bolder, better future
-		for the I-295 corridor, and our city as a whole.
+		for the I-295 corridor and our city as a whole.
 	</p>
 
 	<p class="fst-italic">
@@ -46,12 +46,12 @@
 	<h2 class="text-primary">Program</h2>
 
 	<p>
-		<b class="text-uppercase">Portland Leviathan 2024</b> will feature talks and discussions considering
+		<b class="text-uppercase">PORTLAND LEVIATHAN 2024</b> will feature talks and discussions considering
 		the broad context of urban highways in America, I-295 in particular, the effects on neighborhoods
 		and communities, an overview of Street Light and other data collection methods, current projects,
 		and plans for the future. There will also be a day time tour of the highway from bridge to bridge,
-		and reception to follow. The conference will take place in Spring 2024, exact date to be determined,
-		at the Jewish Community Alliance.
+		and reception to follow. The conference will take place on Thursday, June 2024, at the Jewish Community
+		Alliance, 1342 Congress Street, Portland Maine, 04102.
 	</p>
 
 	<h2 class="text-primary">Presenters</h2>
@@ -62,27 +62,72 @@
 			<span class="fst-italic">Moderator</span>
 		</li>
 		<li class="list-group-item border-0">
-			<div class="fw-bold">Pending Representative</div>
-			<span class="fst-italic">United States Department of Transportation</span>
-		</li>
-		<li class="list-group-item border-0">
-			<div class="fw-bold">Fred Salvucci</div>
-			<span class="fst-italic">Visionary Engineer of the Big Dig</span>
-		</li>
-		<li class="list-group-item border-0">
-			<div class="fw-bold">Julie Lary</div>
-			<span class="fst-italic">Historian, Black Diamond</span>
+			<div class="fw-bold">Julie Larry</div>
+			<span class="fst-italic">On the history of 295 in Portland</span>
 		</li>
 		<li class="list-group-item border-0">
 			<div class="fw-bold">Community Representatives</div>
-			<span class="fst-italic">and more</span>
+			<span class="fst-italic">Eyewitnesses to history</span>
+		</li>
+		<li class="list-group-item border-0">
+			<div class="fw-bold">Dayea Shim (AICP) Program Coordinator, GrowSmart Maine</div>
+			<span class="fst-italic">On highway removal throughout the world</span>
+		</li>
+		<li class="list-group-item border-0">
+			<div class="fw-bold">Ryan Johnson, Associate Urban Planner, Code Studio, Austin TX</div>
+			<span class="fst-italic">A new vision of the 295 corridor</span>
+		</li>
+		<li class="list-group-item border-0">
+			<div class="fw-bold">Beth Osborne, Transportation for America</div>
+		</li>
+		<li class="list-group-item border-0">
+			<div class="fw-bold">Myles Smith, Mainers for Smarter Transportation</div>
+			<span class="fst-italic">On the proposed highway expansion of 295</span>
 		</li>
 	</ul>
+
+	<h2 class="text-primary">Bios</h2>
+
+	<p>
+		<b>Ryan Johnson</b> is an urban planner and designer from Austin, TX. He believes that good design
+		and planning are critical for cities of all sizes to be diverse, active, equitable and engaging places.
+		Before joining Code Studio, Ryan worked in both the public and non-profit sectors on land use planning
+		and active transportation. His experience includes interpreting zoning ordinances, contributing to
+		comprehensive plans and streetscape standards, and participating in multiple affordable housing design
+		competitions. Ryan's research has focused on land use regulation, affordable housing, active transportation,
+		suburban retrofitting, and streetscape design. In his personal life, Ryan serves on the city of Austin's
+		Planning Commission, and enjoys biking, cooking, and traveling to new places.
+	</p>
+	<p>
+		<b>Dayea Shim</b>is senior planner who specializes in connecting community members on planning
+		issues related to transportation, housing, and resiliency.
+	</p>
+	<p>
+		<b>Myles Smith</b> is a longtime community advocate and non-profit leader in both international and
+		community development. Myles's work has focused on supporting journalists and open access to the
+		internet, as well as trade and economic development in Central Asia. He led the Maine Broadband Coalition,
+		an advocacy and public education group devoted to expanding access to high speed internet, particularly
+		in rural areas. Myles is a member of a number of Portland-based and statewide advocacy coalitions
+		for complete streets, cycling safety, and public transportation. He is the volunteer facilitator
+		of an ad hoc group of advocates called Mainers for Smarter Transportation, which is fighting the
+		expansion of the Maine Turnpike from South Portland to Gorham. A native of greater Portland, Myles
+		returned to Portland in 2022 after over two decades in Boston, Washington DC, Russia, Turkmenistan,
+		and Kyrgyzstan.
+	</p>
+	<p>
+		<b>Julie Ann Larry</b>is the former Director of Advocacy at Greater Portland Landmarks. An
+		architectural historian, Julie has researched and presented on the historic development of
+		several of Portland's neighborhoods, including those impacted by I-295 construction and urban
+		renewal projects. Prior to joining Landmarks, Julie was a partner for eighteen years at
+		ttl-architects where she oversaw the restoration of the Portland Observatory, the
+		Castle-in-the-Park at Deering Oaks, and numerous other preservation projects in New England. She
+		is currently the Senior Architectural Historian at Black Diamond Consultants.
+	</p>
 
 	<h2 class="text-primary">Partner Organizations</h2>
 
 	<ul>
-		<li>Portland Society of Architecture</li>
+		<li>Grow Smart Maine</li>
 		<li>Libbytown Neighborhood Association</li>
 		<li>Franklin Reclamation Authority</li>
 		<li>Portland Trails</li>
